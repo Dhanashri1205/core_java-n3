bef4181f1ac3a2e0c6dab06da99e6350d12c3341
Rushi <Rushi@hp>
2023-02-02 11:40:12 +0530

    second commit

---
+++ dayone/src/org/tnsif/intro/DataTypeDemo.java
@@ -0,0 +1,30 @@
+package org.tnsif.intro;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//byte 1 byte
+		byte a=127;
+		
+		//int 2 byte
+		int b=234;
+		
+		//float 4 byte
+		float c=12/5;
+		float d=5/2.5f;
+		
+		//double 4 byte
+		double e=12/5;
+		double f=12d/5d;
+		System.out.println("value of a :"+a);
+		System.out.println("value of b :"+b);
+		System.out.println("value of c :"+c);
+		System.out.println("value of d :"+d);
+		System.out.println("value of e :"+e);
+		System.out.println("value of f :"+f);
+		
+
+	}
+
+}
