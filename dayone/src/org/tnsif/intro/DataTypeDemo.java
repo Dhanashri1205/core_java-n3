a153bbfaf4b2ee7a8db2d829fe3f4b55c2bf9b05
Rushi <Rushi@hp>
2023-02-02 11:58:51 +0530

    second commit

--- dayone/src/org/tnsif/intro/DataTypeDemo.java
+++ dayone/src/org/tnsif/intro/DataTypeDemo.java
@@ -9,6 +9,8 @@ public class DataTypeDemo {
 		
 		//int 2 byte
 		int b=234;
+		long l=1234567l;
+		short s=233;
 		
 		//float 4 byte
 		float c=12/5;
@@ -17,12 +19,31 @@ public class DataTypeDemo {
 		//double 4 byte
 		double e=12/5;
 		double f=12d/5d;
+		
+		//character 1 byte
+		char g='S';
+		//ascii representation
+		int x='1';
+		
+		//String
+		String name="Dhanashri";
+		
+		//boolean
+		boolean h=true;
+		
+		
 		System.out.println("value of a :"+a);
 		System.out.println("value of b :"+b);
 		System.out.println("value of c :"+c);
 		System.out.println("value of d :"+d);
 		System.out.println("value of e :"+e);
 		System.out.println("value of f :"+f);
+		System.out.println("value of g :"+g);
+		System.out.println("value of x :"+x);
+		System.out.println("The name is:  :"+name);
+		System.out.println("value of h :"+h);
+		System.out.println("value of l :"+l);
+		System.out.println("value of s :"+s);
 		
 
 	}
