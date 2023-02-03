8bd931a9fbfa2254be342ce88ac6d7ad9120d485
Rushi <Rushi@hp>
2023-02-03 13:51:34 +0530

    second commit

---
+++ daytwo/src/org/tnsif/looping/WhileDemo.java
@@ -0,0 +1,29 @@
+package org.tnsif.looping;
+
+import java.util.Scanner;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i=1;
+		
+		while(i<=n)
+		{
+			System.out.print(i+" ");
+			i++;
+		}
+		System.out.println("Reverse");
+		int j=n;
+		while(j>=1)
+		{
+			System.out.print(j+" ");
+			j--;
+		}
+		
+
+	}
+
+}
