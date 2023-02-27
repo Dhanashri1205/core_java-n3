962f3c668c84f5f72f9256b0b3cd253b4cc406ca
Rushi <Rushi@hp>
2023-02-27 13:37:41 +0530

    First commit

---
+++ daysixteen/Test/org/tnsif/junit/RepetedTestDemo.java
@@ -0,0 +1,23 @@
+package org.tnsif.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class RepetedTestDemo {
+
+	//@Test
+	@RepeatedTest(5)
+	void test() {
+		int num=1;
+		/*while(num<5)
+		{
+			System.out.println("Helloo Guys!....");
+			num++;
+		}*/
+		System.out.println("Repeated Test Annotation!....");
+		
+	}
+
+}
